ae06f565bb6b059bab5a4a5f3294eb9403ff9cee
Adrián Alonso González <adri14590@gmail.com>
2022-06-30 00:32:02 +0200

    my project settings

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -22,8 +22,8 @@ public class BuildVars {
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static int BUILD_VERSION = 2711;
     public static String BUILD_VERSION_STRING = "8.8.4";
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";;
+    public static int APP_ID = 13807085;
+    public static String APP_HASH = "b8f1274a92e0e3cae6451a20b3b1a0e1";
 
     public static String SMS_HASH = isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
